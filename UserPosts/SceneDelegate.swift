88b6cb1f1b07572968c7ce0f3cf62b65914c447a
Dima Omelchenko <dmytryi.omelchenko@idapgroup.com>
2021-04-02 08:26:50 +0300

    Project init

---
+++ UserPosts/SceneDelegate.swift
@@ -0,0 +1,46 @@
+//
+//  SceneDelegate.swift
+//  UserPosts
+//
+//  Created by Dima Omelchenko on 02.04.2021.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    // MARK: -
+    // MARK: Variables
+
+    var window: UIWindow?
+    
+    // MARK: -
+    // MARK: UIWindowSceneDelegate
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        let contentView = ContentView()
+        
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) { }
+
+    func sceneDidBecomeActive(_ scene: UIScene) { }
+
+    func sceneWillResignActive(_ scene: UIScene) { }
+
+    func sceneWillEnterForeground(_ scene: UIScene) { }
+
+    func sceneDidEnterBackground(_ scene: UIScene) { }
+}
+
